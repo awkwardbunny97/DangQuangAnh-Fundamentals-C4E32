a56e0d86ffcbbe65122e52e7de483077aafae247
awkwardbunny97 <47962647+awkwardbunny97@users.noreply.github.com>
2019-05-18 19:16:53 +0700

    homework

--- Session04/Homework/Assigment/Ex_01.py
+++ Session04/Homework/Assigment/Ex_01.py
@@ -1,10 +1,10 @@
 ############# Danh sách sinh viên ############
 student_list = [
-    {'name':'Nguyễn Văn A' , 'age':'21' , 'address':'Hà Nội' , 'math': 9 , 'physics': 8 , 'chemistry': 7 ,'phone': 12345},
-    {'name':'Nguyễn Văn B' , 'age':'22' , 'address':'Hồ Chí Minhi' , 'math': 5 , 'physics': 6 , 'chemistry': 7 ,'phone': 6789},
-    {'name':'Nguyễn Văn C' , 'age':'23' , 'address':'Dà Nẵng' , 'math': 1 , 'physics': 3 , 'chemistry': 5 ,'phone': 13579},
-    {'name':'Nguyễn Văn D' , 'age':'24' , 'address':'Hòa Bình' , 'math': 2 , 'physics': 4 , 'chemistry': 6 ,'phone': 246810},
-    {'name':'Nguyễn Văn E' , 'age':'25' , 'address':'Vinh' , 'math': 10 , 'physics': 1 , 'chemistry': 5 ,'phone': 987654321},
+    {'name':'Nguyễn Văn A' , 'age':'21' , 'address':'Hà Nội' , 'math': 9 , 'physics': 8 , 'chemistry': 7 ,'phone': ['12345']},
+    {'name':'Nguyễn Văn B' , 'age':'22' , 'address':'Hồ Chí Minhi' , 'math': 5 , 'physics': 6 , 'chemistry': 7 ,'phone': ['6789']},
+    {'name':'Nguyễn Văn C' , 'age':'23' , 'address':'Dà Nẵng' , 'math': 1 , 'physics': 3 , 'chemistry': 5 ,'phone': ['13579']},
+    {'name':'Nguyễn Văn D' , 'age':'24' , 'address':'Hòa Bình' , 'math': 2 , 'physics': 4 , 'chemistry': 6 ,'phone': ['246810']},
+    {'name':'Nguyễn Văn E' , 'age':'25' , 'address':'Vinh' , 'math': 10 , 'physics': 1 , 'chemistry': 5 ,'phone': ['987654321']},
 ]
 
 ############# Điểm Trung Bình ############
@@ -23,12 +23,12 @@ for p in student_list:
 print(p['name'],'là sinh viên có điểm toán cao nhất')
 
 ############# Nhập một số điện thoại và in ra tên sinh viên ############
-phone_number = input("Nhập số điện thoại cần tìm: ")
-student_name = []
-for v in student_list:
-    if phone_number in v['phone']:
-        phone_name = v["name"]
-if phone_name == 0:
-    print("Không ai có sđt này")
+phone = input("Nhập số điện thoại cần tìm: ")
+result = []
+for p in student_list:
+    if phone in p['phone']:
+        result.append(p['name'])
+if len(result) != 0:  
+    print(result)
 else:
-    print("Số điện thoại",phone_number,"là của",phone_name)
+    print("Không có tên  và số điện thoại")
